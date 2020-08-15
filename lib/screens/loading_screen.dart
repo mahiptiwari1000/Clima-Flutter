1bfda3593318f82f423618bcd52d0a163b57c812
Mahip Tiwari <mahiptiwari1000@gmail.com>
2020-08-15 14:48:28 +0530

    Initial Commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,45 @@
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+ @override
+
+ void initState(){
+   super.initState();
+   print("InitState Called");
+   getLocationData();
+ }
+
+  void getLocationData() async {
+  // Geolocator geolocator = Geolocator()
+    // ..forceAndroidLocationManager = true;
+
+   WeatherModel weatherModel = WeatherModel();
+   var weatherData = await weatherModel.getLocationWeather();
+
+   Navigator.push(context, MaterialPageRoute(builder: (context){
+     return LocationScreen(locationWeather: weatherData);
+   }));
+ }
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     body: Center(
+       child: SpinKitDoubleBounce(
+         color: Colors.white,
+         size: 100.0,
+       ),
+     ),
+   );
+  }
+}
